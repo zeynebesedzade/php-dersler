d34537c27771311cb8ea87b419c61fa4def0ef7b
zeynebesedzade <zeynabia-bb104@code.edu.az>
2025-10-16 16:46:46 +0400

    Comparison Operators

---
+++ ders-8/index.php
@@ -0,0 +1,24 @@
+<?php
+
+$a=10;
+$b=20;
+echo $a==$b;
+// false kimi ekrana getdi deyeri 0
+
+echo "<br>";
+$a=10;
+$b=10;
+echo $a==$b;
+// true kimi ekrana getdi deyeri 1
+
+echo "<br>";
+
+$a=10;
+$b=20;
+echo $a!=$b;
+echo "<br>";
+
+$a=10;
+$b="10";
+echo $a===$b;
+//eyni tip deyiller false
