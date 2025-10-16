676c5e4a0e82fa4d501eab9e73c26d41266703e0
zeynebesedzade <zeynabia-bb104@code.edu.az>
2025-10-16 15:25:08 +0400

    data types

---
+++ ders-3/index.php
@@ -0,0 +1,15 @@
+<?php
+$verilen=16.7;
+echo gettype($verilen);
+echo"<br>";
+$verilen=16;
+echo gettype($verilen);
+echo"<br>";
+$verilen=false;
+echo gettype($verilen);
+echo"<br>";
+$verilen=new stdClass();
+echo gettype($verilen);
+echo"<br>";
+$verilen=null;
+echo gettype($verilen);
