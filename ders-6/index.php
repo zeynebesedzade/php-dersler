2a7b0aafe4581e58889afee4c21f173d54b6b4b8
zeynebesedzade <zeynabia-bb104@code.edu.az>
2025-10-16 16:30:12 +0400

    Assignment operators

---
+++ ders-6/index.php
@@ -0,0 +1,29 @@
+<?php
+$number=4;
+echo $number;
+
+echo "<br>";
+
+$number1=4;
+$number1 +=7;
+echo $number1;
+
+echo "<br>";
+
+$ad="Ibrahim";
+$soyad="Memmedov";
+echo $ad.$soyad;
+
+echo "<br>";
+
+$ad="Ibrahim";
+$ad .= " Memmedov";
+echo $ad;
+
+echo "<br>";
+
+$ders="Php dersleri";
+$html="<h1>";
+$html.=$ders;
+$html.="</h1>";
+echo $html;
