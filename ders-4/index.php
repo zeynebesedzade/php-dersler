951a49c30603e16eaab2096e59700ba258f61fbf
zeynebesedzade <zeynabia-bb104@code.edu.az>
2025-10-16 16:07:14 +0400

    constant

---
+++ ders-4/index.php
@@ -0,0 +1,12 @@
+<?php
+define("ad","Nergiz");
+echo ad;
+echo"<br>";
+define("ad1","Nergiz");
+echo ad1;
+echo"<br>";
+define("ad2","Nermin");
+echo ad2;
+echo"<br>";
+define("ad3","Nergiz");
+echo gettype("ad3");
