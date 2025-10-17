2780532f3fe3d45b25831ff80051695098a4209e
zeynebesedzade <zeynabia-bb104@code.edu.az>
2025-10-17 22:13:53 +0400

    Task-6

---
+++ task-6/index.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$age = 100;
+if ($age < 18) {
+    echo "Sən hələ böyüməmisən .";
+} elseif ($age >18 || $age < 60) {
+    echo "Yetkin insansan . ";
+} elseif($age>60){
+echo "Sən təqaüd yaşındasan .";
+}
