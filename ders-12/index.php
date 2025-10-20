c45adf8ba6a7f13020fa6aea925f9cc23380de0b
zeynebesedzade <zeynabia-bb104@code.edu.az>
2025-10-20 18:42:13 +0400

    switch case

---
+++ ders-12/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$url="index";
+switch($url){
+    case "index":
+        echo "index sehifesindesiz";
+        break;
+    case "elaqe":
+        echo "elaqe sehifesindesiz";
+        break;
+    case "haqqimizda":
+        echo "haqqimizda sehifesindesiz";
+        break;
+    default:
+        echo "index sehifesindesiz";
+        break;
+}
+
