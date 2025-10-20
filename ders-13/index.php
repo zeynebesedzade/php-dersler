ab60c54143a9522cf9347f19614c952c41ac91bf
zeynebesedzade <zeynabia-bb104@code.edu.az>
2025-10-20 20:29:30 +0400

    commit

---
+++ ders-13/index.php
@@ -0,0 +1,16 @@
+<?php
+$url="index";
+switch($url):
+    case "index":
+        echo "index sehifesindesiz";
+        break;
+    case "elaqe":
+        echo "elaqe sehifesindesiz";
+        break;
+    case "haqqimizda":
+        echo "haqqimizda sehifesindesiz";
+        break;
+    default:
+        echo "index sehifesindesiz";
+        break;
+endswitch;
