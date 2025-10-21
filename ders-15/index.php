44d69af8e4e96d9d81704da089e24341194d32b1
zeynebesedzade <zeynabia-bb104@code.edu.az>
2025-10-21 13:11:51 +0400

    ic-ice arraylar

---
+++ ders-15/index.php
@@ -0,0 +1,18 @@
+<?php
+$uzvler=[
+    1=>[
+        'ad'=>'Zeyneb',
+        'soyad'=>'Esedzade',
+        'ixtisas'=>'Muellim'
+    ],
+    2=>[
+    'ad'=>'Fatime',
+    'soyad'=>'Esedzade'
+],
+3=>[
+    'ad'=>'Nuray',
+    'soyad'=>'Esedzade'
+]
+];
+echo $uzvler[1]['ad'];
+echo $uzvler[2]['soyad'];
