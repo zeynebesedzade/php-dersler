704d6d23e80f50fbcae74dae954a4d00497a9ce4
zeynebesedzade <zeynabia-bb104@code.edu.az>
2025-10-21 17:59:08 +0400

    While

---
+++ ders-19/index.php
@@ -0,0 +1,19 @@
+<?php
+$a=5;
+while($a<=30){
+    echo $a."<br>";
+    $a+=2;
+}
+echo "<br>";
+$i = 1;
+while ($i <= 10) {
+    echo $i . "<br>";
+    $i++;
+}
+echo "<br>";
+//10dan 1-e qeder azalan sira ile
+$a = 10;
+while ($a >= 1) {
+    echo $a . "<br>";
+    $a--;
+}
