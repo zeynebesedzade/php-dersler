59faa2dde909a4a027c67d7272a42d55bf4745e6
zeynebesedzade <zeynabia-bb104@code.edu.az>
2025-10-21 17:08:36 +0400

    Add equipment details display in index.php

--- ders-18/index.php
+++ ders-18/index.php
@@ -1,1 +1,18 @@
 <?php
+/*$avadanliqlar=[
+    'monitor',
+    'klaviatura',
+    'mouse'
+];
+foreach($avadanliqlar as $a):
+    echo $a."<br>";
+    endforeach;*/
+
+$avadanliqdetallari=[
+    'avadanliqadi'=>'monitor',
+    'marka'=>'samsung',
+    'qiymeti'=>'2000azn'
+];
+foreach($avadanliqdetallari as $a=>$b):
+    echo $a."-----".$b."<br>";
+endforeach;
